3acdc1ad27e8f4427d7d33c73da124edb01a24d4
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2019-07-11 15:07:46 +0200

    Add shadow root check

--- vpu-lit-element.js
+++ vpu-lit-element.js
@@ -3,6 +3,6 @@ import $ from "jquery";
 
 export default class VPULitElement extends LitElement {
     $(selector) {
-        return $(this.shadowRoot.querySelector(selector));
+        return $(this.shadowRoot === null ? this.querySelector(selector) : this.shadowRoot.querySelector(selector));
     }
 }
