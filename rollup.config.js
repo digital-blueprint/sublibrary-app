b07806c2e60d0f3298103dc4fa4912b12c556153
Christoph Reiter <reiter.christoph@gmail.com>
2019-07-17 14:44:54 +0200

    rollup: use serve plugin on watch
    So one doesn't need php to spawn a server.

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import copy from 'rollup-plugin-copy';
 import {terser} from "rollup-plugin-terser";
 import json from 'rollup-plugin-json';
 import replace from "rollup-plugin-replace";
+import serve from 'rollup-plugin-serve';
 
 const build = (typeof process.env.BUILD !== 'undefined') ? process.env.BUILD : 'local';
 console.log("build: " + build);
@@ -42,6 +43,7 @@ export default {
                 'node_modules/select2/dist/css',
             ],
             outputFolder: 'dist/select2'
-        })
+        }),
+        (process.env.ROLLUP_WATCH === 'true') ? serve({contentBase: 'dist', host: '127.0.0.1', port: 8001}) : false
     ]
 };
