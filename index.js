3c1f82265ae4fdb396d60132f2f438da6214b4d2
Christoph Reiter <reiter.christoph@gmail.com>
2019-07-29 16:07:23 +0200

    index: let the node module resolver handle our submodule imports
    No need to hardcode the paths.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import './node_modules/vpu-auth/vpu-auth';
-import './node_modules/vpu-notification/vpu-notification';
-import './node_modules/vpu-knowledge-base-web-page-element-view/vpu-kb-wpe-view';
-import './node_modules/vpu-person-select/person-select';
+import 'vpu-auth/vpu-auth.js';
+import 'vpu-notification/vpu-notification';
+import 'vpu-knowledge-base-web-page-element-view/vpu-kb-wpe-view';
+import 'vpu-person-select/person-select';
 import './library-book-offer-select';
 import './vpu-library-shelving.js';
