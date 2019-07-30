1074147a89354071a0b62cb087a630a235b2683f
Christoph Reiter <reiter.christoph@gmail.com>
2019-07-30 16:09:52 +0200

    Update the included person selector

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'vpu-auth';
 import 'vpu-notification/vpu-notification';
 import 'vpu-knowledge-base-web-page-element-view/vpu-kb-wpe-view';
-import 'vpu-person-select/person-select';
+import 'vpu-person-select';
 import './library-book-offer-select';
 import './vpu-library-shelving.js';
