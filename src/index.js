1eee6ee41920fac0dd2912bf7f0c9a5943b66cb5
Christoph Reiter <reiter.christoph@gmail.com>
2019-08-06 16:16:14 +0200

    Update all submodules
    So we never call customElements.define() directly.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'vpu-auth';
 import 'vpu-notification';
-import 'vpu-knowledge-base-web-page-element-view/vpu-kb-wpe-view';
+import 'vpu-knowledge-base-web-page-element-view';
 import 'vpu-person-select';
 import './library-book-offer-select';
 import './vpu-library-shelving.js';
