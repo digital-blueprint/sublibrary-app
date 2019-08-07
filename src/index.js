17ee1038911adc4f0d6608327c2e7ec879b6d091
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2019-08-07 09:30:02 +0200

    Move LibraryBookOfferSelect to git sub-module

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import 'vpu-auth';
 import 'vpu-notification';
 import 'vpu-knowledge-base-web-page-element-view';
 import 'vpu-person-select';
-import './library-book-offer-select';
+import 'vpu-library-book-offer-select';
 import './vpu-library-shelving.js';
