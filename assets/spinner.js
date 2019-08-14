965d8f62721614e78af17cb2dc4ac6963a4e9471
Christoph Reiter <reiter.christoph@gmail.com>
2019-08-14 11:27:10 +0200

    Don't minify the spinner
    It's not worth it and it's not part of the build process.

--- assets/spinner.js
+++ assets/spinner.js
@@ -1,1 +1,115 @@
-customElements.define("vpu-spinner",class extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"}).innerHTML='\n<style>\n#all-spinner-tuglogo {\n  width:130px;\n    height:130px;\n      position:relative;\n        background-color:transparent;\n          margin:0 auto;\n}\n\n.all-spinner-tuglogo-box {\n  width:20%;\n    height:20%;\n     background-color:#e4154b;\n        position:absolute;\n          top:50%;\n            left:50%;\n          animation-duration: 1.6s;\n            animation-direction:alternate;\n              animation-iteration-count:infinite;\n    animation-fill-mode:both;\n      animation-timing-function:ease;\n        transition: transform 0.5s, background-color 0.2s 0.5s;\n}\n\n#all-spinner-tuglogo-box-1 {\n      animation-name: box1;\n        transform:translateX(-160%) translateY(-50%);\n}\n\n#all-spinner-tuglogo-box-2 {\n      transform-origin:0 0;\n      animation-name: box2;\ntransform:scale(1) translateX(-50%) translateY(-50%);\n}\n\n#all-spinner-tuglogo-box-3 {\n\n      animation-name: box3;\n        animation-delay:0.3s;\n          transform:translateX(60%) translateY(-50%); visibility:visible;\n}\n\n#all-spinner-tuglogo-box-4 {\n\n      animation-name: box4;\n        animation-delay:0.1s;\ntransform:translateX(0%) translateY(-100%);  visibility:visible;\n}\n\n#all-spinner-tuglogo-box-5 {\n\n      animation-name: box5;\n        animation-delay:0.2s;\ntransform:translateX(-100%) translateY(0%);  visibility:visible;\n}\n\n@keyframes box1 {\n    0% { transform:translateX(-50%) translateY(-50%); visibility:hidden; }\n    50% { transform:translateX(-50%) translateY(-50%); visibility:hidden; }\n    70% { transform:translateX(-170%) translateY(-50%); visibility:visible;}\n    80% { transform:translateX(-160%) translateY(-50%); visibility:visible;}\n  100%  { transform:translateX(-160%) translateY(-50%); visibility:visible;}\n}\n\n@keyframes box2 {\n    0% { transform:scale(0) translateX(-50%) translateY(-50%);}\n    5% { transform:scale(0) translateX(-50%) translateY(-50%);}\n    30% { transform:scale(1.2) translateX(-50%) translateY(-50%);}\n    35% { transform:scale(1) translateX(-50%) translateY(-50%);}\n  100% { transform:scale(1) translateX(-50%) translateY(-50%);}\n\n}\n\n@keyframes box3 {\n    0% { transform:translateX(-50%) translateY(-50%); visibility:hidden; }\n    50% { transform:translateX(-50%) translateY(-50%); visibility:hidden; }\n    70% { transform:translateX(70%) translateY(-50%); visibility:visible;}\n    80% { transform:translateX(60%) translateY(-50%); visibility:visible;}\n  100%  { transform:translateX(60%) translateY(-50%); visibility:visible;}\n}\n\n@keyframes box4 {\n    0%  { transform:translateX(-50%) translateY(-50%);  visibility:hidden; }\n    50% { transform:translateX(-50%) translateY(-50%);  visibility:hidden; }\n    70% { transform:translateX(10%) translateY(-110%);  visibility:visible;}\n    80% { transform:translateX(0%) translateY(-100%);  visibility:visible;}\n  100%  { transform:translateX(0%) translateY(-100%);  visibility:visible;}\n}\n@keyframes box5 {\n    0%  { transform:translateX(-50%) translateY(-50%);  visibility:hidden; }\n    50% { transform:translateX(-50%) translateY(-50%);  visibility:hidden; }\n    70% { transform:translateX(-110%) translateY(10%);  visibility:visible;}\n    80% { transform:translateX(-100%) translateY(0%);  visibility:visible;}\n  100%  { transform:translateX(-100%) translateY(0%);  visibility:visible;}\n}\n</style>\n\n<div id="all-spinner-tuglogo">\n  <div id="all-spinner-tuglogo-box-1" class="all-spinner-tuglogo-box"></div>\n  <div id="all-spinner-tuglogo-box-2" class="all-spinner-tuglogo-box"></div>\n  <div id="all-spinner-tuglogo-box-3" class="all-spinner-tuglogo-box"></div>\n  <div id="all-spinner-tuglogo-box-4" class="all-spinner-tuglogo-box"></div>\n  <div id="all-spinner-tuglogo-box-5" class="all-spinner-tuglogo-box"></div>\n</div>'}});
+class Spinner extends HTMLElement {
+
+    constructor() {
+        super();
+        let shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.innerHTML = `
+<style>
+#all-spinner-tuglogo {
+  width:130px;
+    height:130px;
+      position:relative;
+        background-color:transparent;
+          margin:0 auto;
+}
+
+.all-spinner-tuglogo-box {
+  width:20%;
+    height:20%;
+     background-color:#e4154b;
+        position:absolute;
+          top:50%;
+            left:50%;
+          animation-duration: 1.6s;
+            animation-direction:alternate;
+              animation-iteration-count:infinite;
+    animation-fill-mode:both;
+      animation-timing-function:ease;
+        transition: transform 0.5s, background-color 0.2s 0.5s;
+}
+
+#all-spinner-tuglogo-box-1 {
+      animation-name: box1;
+        transform:translateX(-160%) translateY(-50%);
+}
+
+#all-spinner-tuglogo-box-2 {
+      transform-origin:0 0;
+      animation-name: box2;
+transform:scale(1) translateX(-50%) translateY(-50%);
+}
+
+#all-spinner-tuglogo-box-3 {
+
+      animation-name: box3;
+        animation-delay:0.3s;
+          transform:translateX(60%) translateY(-50%); visibility:visible;
+}
+
+#all-spinner-tuglogo-box-4 {
+
+      animation-name: box4;
+        animation-delay:0.1s;
+transform:translateX(0%) translateY(-100%);  visibility:visible;
+}
+
+#all-spinner-tuglogo-box-5 {
+
+      animation-name: box5;
+        animation-delay:0.2s;
+transform:translateX(-100%) translateY(0%);  visibility:visible;
+}
+
+@keyframes box1 {
+    0% { transform:translateX(-50%) translateY(-50%); visibility:hidden; }
+    50% { transform:translateX(-50%) translateY(-50%); visibility:hidden; }
+    70% { transform:translateX(-170%) translateY(-50%); visibility:visible;}
+    80% { transform:translateX(-160%) translateY(-50%); visibility:visible;}
+  100%  { transform:translateX(-160%) translateY(-50%); visibility:visible;}
+}
+
+@keyframes box2 {
+    0% { transform:scale(0) translateX(-50%) translateY(-50%);}
+    5% { transform:scale(0) translateX(-50%) translateY(-50%);}
+    30% { transform:scale(1.2) translateX(-50%) translateY(-50%);}
+    35% { transform:scale(1) translateX(-50%) translateY(-50%);}
+  100% { transform:scale(1) translateX(-50%) translateY(-50%);}
+
+}
+
+@keyframes box3 {
+    0% { transform:translateX(-50%) translateY(-50%); visibility:hidden; }
+    50% { transform:translateX(-50%) translateY(-50%); visibility:hidden; }
+    70% { transform:translateX(70%) translateY(-50%); visibility:visible;}
+    80% { transform:translateX(60%) translateY(-50%); visibility:visible;}
+  100%  { transform:translateX(60%) translateY(-50%); visibility:visible;}
+}
+
+@keyframes box4 {
+    0%  { transform:translateX(-50%) translateY(-50%);  visibility:hidden; }
+    50% { transform:translateX(-50%) translateY(-50%);  visibility:hidden; }
+    70% { transform:translateX(10%) translateY(-110%);  visibility:visible;}
+    80% { transform:translateX(0%) translateY(-100%);  visibility:visible;}
+  100%  { transform:translateX(0%) translateY(-100%);  visibility:visible;}
+}
+@keyframes box5 {
+    0%  { transform:translateX(-50%) translateY(-50%);  visibility:hidden; }
+    50% { transform:translateX(-50%) translateY(-50%);  visibility:hidden; }
+    70% { transform:translateX(-110%) translateY(10%);  visibility:visible;}
+    80% { transform:translateX(-100%) translateY(0%);  visibility:visible;}
+  100%  { transform:translateX(-100%) translateY(0%);  visibility:visible;}
+}
+</style>
+
+<div id="all-spinner-tuglogo">
+  <div id="all-spinner-tuglogo-box-1" class="all-spinner-tuglogo-box"></div>
+  <div id="all-spinner-tuglogo-box-2" class="all-spinner-tuglogo-box"></div>
+  <div id="all-spinner-tuglogo-box-3" class="all-spinner-tuglogo-box"></div>
+  <div id="all-spinner-tuglogo-box-4" class="all-spinner-tuglogo-box"></div>
+  <div id="all-spinner-tuglogo-box-5" class="all-spinner-tuglogo-box"></div>
+</div>`;
+    }
+}
+
+if (!customElements.get('vpu-spinner'))
+    customElements.define('vpu-spinner', Spinner);
