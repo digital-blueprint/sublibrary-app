1867352c2919784b53b641d2d68cfc9080e1acaa
Christoph Reiter <reiter.christoph@gmail.com>
2019-09-25 14:39:37 +0200

    Add a test for vpu-library-app

--- test/unit.js
+++ test/unit.js
@@ -1,10 +1,29 @@
 import '../src/vpu-library-shelving';
+import '../src/vpu-library-app.js';
 
 describe('vpu-library-shelving basics', () => {
   let node;
 
   beforeEach(async () => {
     node = document.createElement('vpu-library-shelving');
+    document.body.appendChild(node);
+    await node.updateComplete;
+  });
+
+  afterEach(() => {
+    node.remove();
+  });
+
+  it('should render', () => {
+      expect(node).to.have.property('shadowRoot');
+  });
+});
+
+describe('vpu-library-app basics', () => {
+  let node;
+
+  beforeEach(async () => {
+    node = document.createElement('vpu-library-app');
     document.body.appendChild(node);
     await node.updateComplete;
   });
