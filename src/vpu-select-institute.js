460fb8f27cfe1e2ba0a3b6d2a77095b1c93c1c37
Eugen Neuber <eugen.neuber@tugraz.at>
2019-10-30 13:31:32 +0100

    Add vpu-select-institute
    TODO: styling is not TUGraz compatible

---
+++ src/vpu-select-institute.js
@@ -0,0 +1,118 @@
+//import $ from 'jquery';
+import {i18n} from './i18n.js';
+import {css, html} from 'lit-element';
+import {send as notify} from 'vpu-notification';
+import * as commonUtils from 'vpu-common/utils';
+import * as commonStyles from 'vpu-common/styles';
+import * as errorUtils from "vpu-common/error";
+import VPULitElement from "vpu-common/vpu-lit-element";
+import * as utils from "./utils";
+
+class SelectInstitute extends VPULitElement {
+    constructor() {
+        super();
+        this.lang = 'de';
+        this.select = null;
+        this.institutes = [];
+        this.institute = 0;
+    }
+
+    static get properties() {
+        return {
+            lang: {type: String},
+            select: {type: Object, attribute: false},
+            institutes: {type: Array, attribute: false},
+            institute: {type: String, attribute: false},
+        }
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.updateComplete.then(()=> {
+            this.select = this._('#select-1');
+            window.addEventListener("vpu-auth-person-init", () => {
+                this.institutes = this.getAssosiatedInstitutes();
+                this.institute = this.institutes.length > 0 ? this.institutes[0] : '';
+                window.VPUPersonLibrary = this.institute;
+                console.log('(init) window.VPUPersonLibrary = ' + window.VPUPersonLibrary);
+            });
+        });
+    }
+
+    update(changedProperties) {
+        changedProperties.forEach((oldValue, propName) => {
+            if (propName === "lang") {
+                i18n.changeLanguage(this.lang);
+            }
+        });
+
+        super.update(changedProperties);
+    }
+
+    onLanguageChanged(e) {
+        this.lang = e.detail.lang;
+    }
+
+    changed() {
+        this.institute = this.select.options[this.select.selectedIndex].innerText;
+        window.VPUPersonLibrary = this.institute;
+        console.log('(change) window.VPUPersonLibrary = ' + window.VPUPersonLibrary);
+    }
+
+    static get styles() {
+        // language=css
+        return css`
+            ${commonStyles.getThemeCSS()}
+            ${commonStyles.getGeneralCSS()}
+            ${commonStyles.getNotificationCSS()}
+            #select-1 {
+                border-radius: var(--vpu-border-radius);
+                color: inherit;
+                background: inherit;
+                margin-left: 20px;
+            }
+        `;
+    }
+
+    render() {
+        return html`
+        <select id="select-1" class="select" ?disabled=${this.institutes.length < 2} @change="${this.changed}">
+        ${ this.institutes.map((item) => { return html`<option value=${item} ?selected=${item === this.institute}>${item}</option>`;})}
+        </select>
+        `;
+    }
+
+    /**
+     * Returns the list of assigned libraries of the current user
+     *
+     * @returns {Array}
+     */
+    getAssosiatedInstitutes() {
+        if (window.VPUPerson === undefined) {
+            return [];
+        }
+
+        const functions = window.VPUPerson.functions;
+
+        if (functions === undefined) {
+            return [];
+        }
+
+        const re = /^F_BIB:F:(\d+):\d+$/;
+        let results = [];
+
+        for (const item of functions) {
+            const matches = re.exec(item);
+
+            if (matches !== null) {
+                results.push("F" + matches[1]);
+            }
+        }
+
+        return results;
+    };
+
+}
+
+commonUtils.defineCustomElement('vpu-select-institute', SelectInstitute);
