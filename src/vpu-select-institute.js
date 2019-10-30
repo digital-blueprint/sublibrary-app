9ff1664211f513c8e57cbaf11421db993346d5d8
Eugen Neuber <eugen.neuber@tugraz.at>
2019-10-30 17:03:15 +0100

    Use jQuery Select2 as select box
    see issue #7
    TODO: dropdown has no proper background ?
    TODO: options have bullets (like list elements) ?

--- src/vpu-select-institute.js
+++ src/vpu-select-institute.js
@@ -1,26 +1,30 @@
-//import $ from 'jquery';
+import $ from 'jquery';
+import select2 from 'select2';
+import select2CSSPath from 'select2/dist/css/select2.min.css';
 import {i18n} from './i18n.js';
 import {css, html} from 'lit-element';
-import {send as notify} from 'vpu-notification';
 import * as commonUtils from 'vpu-common/utils';
 import * as commonStyles from 'vpu-common/styles';
-import * as errorUtils from "vpu-common/error";
-import VPULitElement from "vpu-common/vpu-lit-element";
-import * as utils from "./utils";
+import select2LangDe from "vpu-person-select/src/i18n/de/select2";
+import select2LangEn from "vpu-person-select/src/i18n/en/select2";
+import VPULitElementJQuery from "vpu-common/vpu-lit-element-jquery";
 
-class SelectInstitute extends VPULitElement {
+select2(window, $);
+
+class SelectInstitute extends VPULitElementJQuery {
     constructor() {
         super();
         this.lang = 'de';
-        this.select = null;
+        this.$select = null;
         this.institutes = [];
         this.institute = 0;
+        // For some reason using the same ID on the whole page twice breaks select2 (regardless if they are in different custom elements)
+        this.selectId = 'select-institute-' + commonUtils.makeId(24);
     }
 
     static get properties() {
         return {
             lang: {type: String},
-            select: {type: Object, attribute: false},
             institutes: {type: Array, attribute: false},
             institute: {type: String, attribute: false},
         }
@@ -30,41 +34,80 @@ class SelectInstitute extends VPULitElement {
         super.connectedCallback();
 
         this.updateComplete.then(()=> {
-            this.select = this._('#select-1');
+            this.$select = this.$('#' + this.selectId);
+
+            // close the selector on blur of the web component
+            // $(this).blur(() => {
+            //     // the 500ms delay is a workaround to actually get an item selected when clicking on it,
+            //     // because the blur gets also fired when clicking in the selector
+            //     setTimeout(() => {
+            //         if (this.select2IsInitialized()) {
+            //             this.$select.select2('close');
+            //         }
+            //     }, 500);
+            // });
+
             window.addEventListener("vpu-auth-person-init", () => {
                 this.institutes = this.getAssosiatedInstitutes();
                 this.institute = this.institutes.length > 0 ? this.institutes[0] : '';
                 window.VPUPersonLibrary = this.institute;
                 console.log('(init) window.VPUPersonLibrary = ' + window.VPUPersonLibrary);
+                this.initSelect2();
             });
         });
+    }
+
+    initSelect2(ignorePreset = false) {
+        const that = this;
+        const $this = $(this);
+
+        if (this.$select === null) {
+            return false;
+        }
+
+        // we need to destroy Select2 and remove the event listeners before we can initialize it again
+        if (this.$select && this.$select.hasClass('select2-hidden-accessible')) {
+            this.$select.select2('destroy');
+            this.$select.off('select2:select');
+            this.$select.off('select2:closing');
+        }
+
+        this.$select.select2({
+            width: '100%',
+            language: this.lang === "de" ? select2LangDe() : select2LangEn(),
+            placeholderOption: 'select an institute', //i18n.t('no institute found'),
+            dropdownParent: this._('#select-institute-dropdown'),
+            data: this.institutes.map((item, id) => { return {'id': item, 'text': item}; }),
+        }).on("select2:select", function (e) {
+            //debugger
+            if (that.$select ) {
+                that.institute = that.$select.select2('data')[0].text;
+                window.VPUPersonLibrary = that.institute;
+                console.log('(change) window.VPUPersonLibrary = ' + window.VPUPersonLibrary);
+            }
+        });
+
+        this.$select.css('visibility', 'visible');
+        return true;
+    }
+
+    select2IsInitialized() {
+        return this.$select !== null && this.$select.hasClass("select2-hidden-accessible");
     }
 
     update(changedProperties) {
         changedProperties.forEach((oldValue, propName) => {
             if (propName === "lang") {
                 i18n.changeLanguage(this.lang);
+                if (this.select2IsInitialized()) {
+                    // no other way to set an other language at runtime did work
+                    this.initSelect2(true);
+                }
             }
         });
 
         super.update(changedProperties);
     }
-
-    onLanguageChanged(e) {
-        this.lang = e.detail.lang;
-    }
-
-    changed() {
-        this.institute = this.select.options[this.select.selectedIndex].innerText;
-        window.VPUPersonLibrary = this.institute;
-        console.log('(change) window.VPUPersonLibrary = ' + window.VPUPersonLibrary);
-    }
-    //
-    // selectOpen() {
-    //     var e = document.createEvent('MouseEvents');
-    //     e.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    //     this.select.dispatchEvent(e);
-    // }
 
     static get styles() {
         // language=css
@@ -72,28 +115,42 @@ class SelectInstitute extends VPULitElement {
             ${commonStyles.getThemeCSS()}
             ${commonStyles.getGeneralCSS()}
             ${commonStyles.getNotificationCSS()}
-            select, option {
-              -webkit-appearance: none; /* WebKit/Chromium */
-              -moz-appearance: none; /* Gecko */
-              appearance: none; /* future (or now) */
-              border: 0;
+            
+            .select2-control.control {
+                width: 75%;
             }
-            #select-1 {
+
+            .select2-dropdown {
                 border-radius: var(--vpu-border-radius);
-                color: inherit;
-                background: inherit;
-                margin-left: 20px;
+                background: white;
             }
+
+            .select2-container--default .select2-selection--single {
+                border-radius: var(--vpu-border-radius);
+            }
+
+            .select2-container--default .select2-selection--single .select2-selection__rendered {
+                color: inherit;
+            }
+            
         `;
     }
 
     render() {
+        commonUtils.initAssetBaseURL('vpu-select-institute-src');
+        const select2CSS = commonUtils.getAssetURL(select2CSSPath);
         return html`
-        <select id="select-1" ?disabled=${this.institutes.length < 2} @change="${this.changed}">
-        ${ this.institutes.map((item) => { return html`<option value=${item} ?selected=${item === this.institute}>${item}</option>`;})}
-        </select>
+            <link rel="stylesheet" href="${select2CSS}">
+
+        <div class="select">
+            <div class="select2-control control">
+                <!-- https://select2.org-->
+                <select id="${this.selectId}" name="select-institute" class="select" style="visibility: hidden;">
+            </div>
+            <div id="select-institute-dropdown"></div>
+        </div>
+
         `;
-//    <vpu-icon name="chevron-down" id="menu-chevron-icon" @click="${this.selectOpen}"></vpu-icon>
     }
 
     /**
