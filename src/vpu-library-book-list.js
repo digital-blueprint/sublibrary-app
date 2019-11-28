47d3ee96f5087db695a2a0a9b2f21f0308e8fe2b
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2019-11-28 12:07:46 +0100

    Add location selector and improve book list loading

--- src/vpu-library-book-list.js
+++ src/vpu-library-book-list.js
@@ -5,6 +5,13 @@ import * as commonUtils from 'vpu-common/utils';
 import * as commonStyles from 'vpu-common/styles';
 import 'vpu-data-table-view';
 import * as errorUtils from "vpu-common/error";
+import select2 from 'select2';
+import select2LangDe from './i18n/de/select2'
+import select2LangEn from './i18n/en/select2'
+import select2CSSPath from 'select2/dist/css/select2.min.css';
+import $ from "jquery";
+
+select2(window, $);
 
 class LibraryBookList extends VPULibraryLitElement {
     constructor() {
@@ -16,6 +23,10 @@ class LibraryBookList extends VPULibraryLitElement {
         this.books = [];
         this.organizationId = '';
         this.abortController = null;
+        this.locationIdentifier = '';
+        this.locationIdentifiers = [];
+        this.locationIdentifierSelectId = 'vpu-library-book-list-location-identifier-select-' + commonUtils.makeId(24);
+        this.$locationIdentifierSelect = null;
     }
 
     /**
@@ -27,6 +38,8 @@ class LibraryBookList extends VPULibraryLitElement {
             entryPointUrl: { type: String, attribute: 'entry-point-url' },
             organizationId: { type: String, attribute: 'organization-id' },
             books: { type: Object, attribute: false },
+            locationIdentifiers: { type: Array, attribute: false },
+            locationIdentifier: { type: String, attribute: false },
         };
     }
 
@@ -34,6 +47,8 @@ class LibraryBookList extends VPULibraryLitElement {
         super.connectedCallback();
 
         this.updateComplete.then(()=>{
+            this.initLocationIdentifierSelect();
+
             // show user interface when logged in person object is available
             this.callInitUserInterface();
 
@@ -53,15 +68,28 @@ class LibraryBookList extends VPULibraryLitElement {
         changedProperties.forEach((oldValue, propName) => {
             if (propName === "lang") {
                 i18n.changeLanguage(this.lang);
+                this.updateLocationIdentifierSelect();
 
                 // we need to update the column titles
-                this.loadTable();
+                this.buildTable(false);
             } else if (propName === "organizationId") {
                 this.loadTable();
+            } else if (propName === "locationIdentifiers") {
+                this.updateLocationIdentifierSelect();
+            } else if (propName === "locationIdentifier") {
+                this.buildTable(false);
             }
         });
 
         super.update(changedProperties);
+    }
+
+    updateLocationIdentifierSelect() {
+        this.initLocationIdentifierSelect();
+
+        // if (this.$locationIdentifierSelect !== null) {
+        //     this.$locationIdentifierSelect.trigger('change.select2');
+        // }
     }
 
     loadTable() {
@@ -103,47 +131,99 @@ class LibraryBookList extends VPULibraryLitElement {
             })
             .then(result => {
                 that.books = result['hydra:member'];
-
-                if (that.books.length > 0) {
-                    const vdtv1 = that._('#book-books-1');
-                    if (vdtv1 !== null) {
-                        const columns = [
-                            {title: i18n.t('book-list.book-title') },
-                            {title: i18n.t('book-list.book-author') },
-                            {title: i18n.t('book-list.book-barcode') },
-                            {title: i18n.t('book-list.book-location') },
-                            {title: i18n.t('book-list.book-location-identifier') },
-                        ];
-                        // const vdtv1_columnDefs = [
-                        //     {targets: [2], visible: false},
-                        //     {targets: [1], orderData: [2]},
-                        //     {targets: [2, 3], searchable: false},
-                        //     {targets: [3], sortable: false}
-                        // ];
-                        const tbl = [];
-                        that.books.forEach(function(bookOffer) {
-                            const row = [
-                                bookOffer.book.title,
-                                bookOffer.book.author,
-                                bookOffer.barcode,
-                                bookOffer.location,
-                                bookOffer.locationIdentifier,
-                            ];
-                            tbl.push(row);
-                        });
-                        vdtv1.set_columns(columns)
-                            // .set_columnDefs(vdtv1_columnDefs)
-                            .set_datatable(tbl);
-                    }
-                    $bookListBlock.show();
-                } else {
-                    $noBooksBlock.show();
-                }
+                that.buildTable();
             }).catch(error => errorUtils.handleFetchError(error, i18n.t('renew-book.error-load-books-summary')));
+    }
+
+    buildTable(updateLocationIdentifiers = true) {
+        const that = this;
+        const $bookListBlock = this.$('#book-list-block');
+        const $noBooksBlock = this.$('#no-books-block');
+        let locationIdentifiers = [];
+
+        if (this.books.length > 0) {
+            const vdtv1 = this._('#book-books-1');
+            if (vdtv1 !== null) {
+                const columns = [
+                    {title: i18n.t('book-list.book-title')},
+                    {title: i18n.t('book-list.book-author')},
+                    {title: i18n.t('book-list.book-barcode')},
+                    {title: i18n.t('book-list.book-location')},
+                    {title: i18n.t('book-list.book-location-identifier')},
+                ];
+                // const vdtv1_columnDefs = [
+                //     {targets: [2], visible: false},
+                //     {targets: [1], orderData: [2]},
+                //     {targets: [2, 3], searchable: false},
+                //     {targets: [3], sortable: false}
+                // ];
+                const tbl = [];
+                this.books.forEach(function (bookOffer) {
+                    if (that.locationIdentifier === "" ||
+                        that.locationIdentifier === bookOffer.locationIdentifier) {
+                        const row = [
+                            bookOffer.book.title,
+                            bookOffer.book.author,
+                            bookOffer.barcode,
+                            bookOffer.location,
+                            bookOffer.locationIdentifier,
+                        ];
+
+                        tbl.push(row);
+                    }
+
+                    if (updateLocationIdentifiers &&
+                        bookOffer.locationIdentifier !== "" &&
+                        !locationIdentifiers.includes(bookOffer.locationIdentifier)) {
+                        locationIdentifiers.push(bookOffer.locationIdentifier);
+                    }
+                });
+                vdtv1.set_columns(columns)
+                // .set_columnDefs(vdtv1_columnDefs)
+                    .set_datatable(tbl);
+            }
+            $bookListBlock.show();
+        } else {
+            $noBooksBlock.show();
+        }
+
+        if (updateLocationIdentifiers) {
+            this.locationIdentifiers = locationIdentifiers.sort();
+            this.locationIdentifier = "";
+
+            if (this.$locationIdentifierSelect !== null) {
+                this.$locationIdentifierSelect.val("");
+            }
+        }
     }
 
     onLanguageChanged(e) {
         this.lang = e.detail.lang;
+    }
+
+    /**
+     * Initializes the locationIdentifier Select2 selector
+     */
+    initLocationIdentifierSelect() {
+        let that = this;
+        this.$locationIdentifierSelect = this.$('#' + this.locationIdentifierSelectId);
+
+        // destroy previous instance of Select2
+        if (this.$locationIdentifierSelect.hasClass("select2-hidden-accessible")) {
+            this.$locationIdentifierSelect.select2("destroy");
+        }
+
+        this.$locationIdentifierSelect.select2({
+            width: '100%',
+            allowClear: true,
+            language: this.lang === "de" ? select2LangDe() : select2LangEn(),
+            placeholder: i18n.t('book-list.location-identifier-select-placeholder'),
+            dropdownParent: this.$('#location-identifier-select-dropdown'),
+        }).on("select2:select", function (e) {
+            that.locationIdentifier = e.params.data.id;
+        }).on("select2:clear", function (e) {
+            that.locationIdentifier = "";
+        });
     }
 
     static get styles() {
@@ -161,15 +241,34 @@ class LibraryBookList extends VPULibraryLitElement {
     }
 
     render() {
+        let locationIdentifierItemTemplates = [];
+        this.locationIdentifiers.forEach((item) => {
+            locationIdentifierItemTemplates.push(html`<option value="${item}">${item}</option>`);
+        });
+
+        commonUtils.initAssetBaseURL('vpu-library-book-list-src');
+        const select2CSS = commonUtils.getAssetURL(select2CSSPath);
         return html`
+            <link rel="stylesheet" href="${select2CSS}">
             <form class="hidden">
                 <vpu-mini-spinner id="books-loading" style="font-size: 2em; display: none;"></vpu-mini-spinner>
-                <div id="book-list-block" class="field">
-                    <label class="label">${i18n.t('book-list.books')}</label>
-                    <div class="control">
-                        <vpu-data-table-view searching paging lang="${this.lang}" id="book-books-1" columns-count="5"></vpu-data-table-view>
+                <div id="book-list-block">
+                    <div class="field">
+                        <label class="label">${i18n.t('book-list.book-location-identifier')}</label>
+                        <div class="control">
+                            <select id="${this.locationIdentifierSelectId}">
+                                <option value=""></option>
+                                ${locationIdentifierItemTemplates}
+                            </select>
+                            <div id="location-identifier-select-dropdown"></div>
+                        </div>
                     </div>
-                </div>
+                    <div class="field">
+                        <label class="label">${i18n.t('book-list.books')}</label>
+                        <div class="control">
+                            <vpu-data-table-view searching paging lang="${this.lang}" id="book-books-1" columns-count="5"></vpu-data-table-view>
+                        </div>
+                    </div>
                 <div id="no-books-block">
                     ${i18n.t('book-list.no-books')}
                 </div>
