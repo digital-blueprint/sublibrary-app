74c3e9e3ad2c3436999b87d1892c11482c1ca9bf
Christoph Reiter <reiter.christoph@gmail.com>
2020-01-02 10:39:18 +0100

    Use source-sans-pro from npm
    It contains the newest version and all formats we could want.
    Also switch to woff2 since we don't need IE support.

--- rollup.config.js
+++ rollup.config.js
@@ -171,7 +171,7 @@ export default {
                 {src: 'assets/htaccess-shared', dest: 'dist/shared/', rename: '.htaccess'},
                 {src: 'assets/*.css', dest: 'dist/local/' + pkg.name},
                 {src: 'assets/*.ico', dest: 'dist/local/' + pkg.name},
-                {src: 'assets/fonts/*', dest: 'dist/local/' + pkg.name + '/fonts'},
+                {src: 'node_modules/source-sans-pro/WOFF2/TTF/*', dest: 'dist/local/' + pkg.name + '/fonts'},
                 {src: 'node_modules/vpu-common/vpu-spinner.js', dest: 'dist/local/' + pkg.name, rename: 'spinner.js'},
                 {src: 'assets/icon-*.png', dest: 'dist/local/' + pkg.name},
                 {src: 'assets/manifest.json', dest: 'dist', rename: pkg.name + '.manifest.json'},
