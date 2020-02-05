bc0a3a43e0af58bbcc9909cb85d29c1473be9202
Christoph Reiter <reiter.christoph@gmail.com>
2020-02-05 16:08:36 +0100

    Add a license checker with a whitelist
    * Only consider production deps which end up in the bundle (also move our internal deps from dev for this)
    * whitelist various licenses but skip our internal packages which don't have a license yet.

--- rollup.config.js
+++ rollup.config.js
@@ -79,7 +79,8 @@ switch (build) {
 
 
 const CHUNK_BLACKLIST = [
-  'jszip'  // jszip is a node module by default and rollup chunking is confused by that and emits warnings
+  'jszip',  // jszip is a node module by default and rollup chunking is confused by that and emits warnings
+  'source-sans-pro'
 ];
 
 /**
