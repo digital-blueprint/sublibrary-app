digital-blueprint/sublibrary-app

commit b8c234576d9e97f25056bea5760b327be3bb202d

Migrate Matomo events to provider

src/dbp-library-book-list.js
@@ -5,7 +5,6 @@ import {LibraryElement} from "./library-element.js";
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import {DataTableView} from '@dbp-toolkit/data-table-view';
-import * as errorUtils from "@dbp-toolkit/common/error";
 import {MiniSpinner} from "@dbp-toolkit/common";
 import select2 from 'select2';
 import select2LangDe from './i18n/de/select2';
@@ -202,7 +201,7 @@ class LibraryBookList extends ScopedElementsMixin(LibraryElement) {
                 that.buildTable();
                 $booksLoadingIndicator.hide();
             }).catch(error => {
-                errorUtils.handleFetchError(error, i18n.t('book-list.error-load-books'));
+                this.handleFetchError(error, i18n.t('book-list.error-load-books'));
                 $booksLoadingIndicator.hide();
             });
     }

src/dbp-library-budget.js
@@ -5,7 +5,6 @@ import {ScopedElementsMixin} from '@open-wc/scoped-elements';
 import {LibraryElement} from "./library-element.js";
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
-import * as errorUtils from "@dbp-toolkit/common/error";
 import {OrganizationSelect} from './organization-select.js';
 import {MiniSpinner} from '@dbp-toolkit/common';
 import {classMap} from 'lit-html/directives/class-map.js';
@@ -143,7 +142,7 @@ class LibraryBudget extends ScopedElementsMixin(LibraryElement) {
                     that.pageStatus = pageStatus.noBudget;
                 } else {
                     that.pageStatus = pageStatus.none;
-                    errorUtils.handleFetchError(error, i18n.t('budget.load-error'));
+                    this.handleFetchError(error, i18n.t('budget.load-error'));
                 }
             });
     }

src/dbp-library-loan-list.js
@@ -5,7 +5,6 @@ import {LibraryElement} from "./library-element.js";
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import {DataTableView} from '@dbp-toolkit/data-table-view';
-import * as errorUtils from "@dbp-toolkit/common/error";
 import {OrganizationSelect} from './organization-select.js';
 import {MiniSpinner, Button} from '@dbp-toolkit/common';
 import {classMap} from 'lit-html/directives/class-map.js';
@@ -166,7 +165,7 @@ class LibraryLoanList extends ScopedElementsMixin(LibraryElement) {
                 that.buildTable();
                 $loansLoadingIndicator.hide();
             }).catch(error => {
-                errorUtils.handleFetchError(error, i18n.t('loan-list.error-load-loans'));
+                this.handleFetchError(error, i18n.t('loan-list.error-load-loans'));
                 $loansLoadingIndicator.hide();
             });
     }
@@ -344,7 +343,7 @@ class LibraryLoanList extends ScopedElementsMixin(LibraryElement) {
 
                         // open mail client with new mail
                         location.href = `mailto:${loan.borrower.email}?subject=${subject}`;
-                    }).catch(error => errorUtils.handleFetchError(error, i18n.t('loan-list.error-load-loan')));
+                    }).catch(error => { this.handleFetchError(error, i18n.t('loan-list.error-load-loan')); });
                 break;
             }
         }

src/dbp-library-order-list.js
@@ -6,7 +6,6 @@ import {LibraryElement} from "./library-element.js";
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import {DataTableView} from '@dbp-toolkit/data-table-view';
-import * as errorUtils from "@dbp-toolkit/common/error";
 import {OrganizationSelect} from './organization-select.js';
 import {MiniSpinner} from '@dbp-toolkit/common';
 import {classMap} from 'lit-html/directives/class-map.js';
@@ -241,7 +240,7 @@ class LibraryOrderList extends ScopedElementsMixin(LibraryElement) {
 
                 $booksLoadingIndicator.hide();
             }).catch(error => {
-                errorUtils.handleFetchError(error, i18n.t('order-list.error-load-orders'));
+                this.handleFetchError(error, i18n.t('order-list.error-load-orders'));
                 $booksLoadingIndicator.hide();
             });
     }

src/dbp-library-renew-loan.js
@@ -8,7 +8,6 @@ import {PersonSelect} from '@dbp-toolkit/person-select';
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import {DataTableView} from '@dbp-toolkit/data-table-view';
-import * as errorUtils from "@dbp-toolkit/common/error";
 import {OrganizationSelect} from './organization-select.js';
 import {MiniSpinner, Button} from '@dbp-toolkit/common';
 import {classMap} from 'lit-html/directives/class-map.js';
@@ -224,7 +223,7 @@ class LibraryRenewLoan extends ScopedElementsMixin(LibraryElement) {
                     }
                     $loansLoadingIndicator.hide();
                 }).catch(error => {
-                    errorUtils.handleFetchError(error, i18n.t('renew-loan.error-load-loans-summary'));
+                    this.handleFetchError(error, i18n.t('renew-loan.error-load-loans-summary'));
                     $loansLoadingIndicator.hide();
                 });
 
@@ -348,7 +347,7 @@ class LibraryRenewLoan extends ScopedElementsMixin(LibraryElement) {
                         });
                         dateSelect.value = commonUtils.dateToInputDateString(loan.endTime);
                         timeSelect.value = commonUtils.dateToInputTimeString(loan.endTime);
-                    }).catch(error => errorUtils.handleFetchError(error, i18n.t('renew-loan.error-renew-loan-summary')))
+                    }).catch(error => { this.handleFetchError(error, i18n.t('renew-loan.error-renew-loan-summary')); })
                     .finally(() => { button.stop(); });
                 break;
             }

src/dbp-library-return-book.js
@@ -5,7 +5,6 @@ import {ScopedElementsMixin} from '@open-wc/scoped-elements';
 import {LibraryElement} from "./library-element.js";
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
-import * as errorUtils from "@dbp-toolkit/common/error";
 import {OrganizationSelect} from './organization-select.js';
 import {MiniSpinner, Button} from '@dbp-toolkit/common';
 import {classMap} from 'lit-html/directives/class-map.js';
@@ -128,7 +127,7 @@ class LibraryReturnBook extends ScopedElementsMixin(LibraryElement) {
                             "body": i18nKey('return-book.error-no-existing-loans-body'),
                         };
                     }
-                }).catch(error => errorUtils.handleFetchError(error, i18n.t('renew-loan.error-load-loans-summary')));
+                }).catch(error => { this.handleFetchError(error, i18n.t('renew-loan.error-load-loans-summary')); });
             }).on('unselect', function (e) {
                 $returnBookBlock.hide();
             });
@@ -157,7 +156,7 @@ class LibraryReturnBook extends ScopedElementsMixin(LibraryElement) {
                             "body": i18n.t('return-book.success-body', {personName: that.borrowerName}),
                         };
                     },
-                    error: errorUtils.handleXhrError,
+                    error: (jqXHR, textStatus, errorThrown) => { this.handleXhrError(jqXHR, textStatus, errorThrown); },
                     complete: function (jqXHR, textStatus, errorThrown) {
                         that._("#send").stop();
                         that.updateSubmitButtonDisabled();

src/dbp-library-shelving.js
@@ -8,7 +8,6 @@ import Suggestions from 'suggestions';
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import suggestionsCSSPath from 'suggestions/dist/suggestions.css';
-import * as errorUtils from "@dbp-toolkit/common/error";
 import {Button, MiniSpinner} from "@dbp-toolkit/common";
 import {OrganizationSelect} from './organization-select.js';
 import {classMap} from 'lit-html/directives/class-map.js';
@@ -153,7 +152,7 @@ class LibraryShelving extends ScopedElementsMixin(LibraryElement) {
 
                         $bookOfferSelect[0].clear();
                     },
-                    error: errorUtils.handleXhrError,
+                    error: (jqXHR, textStatus, errorThrown) => { this.handleXhrError(jqXHR, textStatus, errorThrown); },
                     complete: function (jqXHR, textStatus, errorThrown) {
                         that._("#send").stop();
                     }

src/library-book-offer-select.js
@@ -10,7 +10,6 @@ import {createI18nInstance} from './i18n.js';
 import {Icon} from "@dbp-toolkit/common";
 import * as commonUtils from "@dbp-toolkit/common/utils";
 import * as commonStyles from '@dbp-toolkit/common/styles';
-import * as errorUtils from "@dbp-toolkit/common/error";
 import select2CSSPath from 'select2/dist/css/select2.min.css';
 import {AdapterLitElement} from "@dbp-toolkit/provider/src/adapter-lit-element";
 
@@ -196,7 +195,7 @@ export class LibraryBookOfferSelect extends ScopedElementsMixin(AdapterLitElemen
                         results: results
                     };
                 },
-                error: errorUtils.handleXhrError
+                error: (jqXHR, textStatus, errorThrown) => { this.handleXhrError(jqXHR, textStatus, errorThrown); }
             }
         }).on("select2:select", function (e) {
             that.$('#library-book-offer-select-dropdown').removeClass('select2-bug');

commit 1649c1a494e0f217bd919b0a1fb70c6f6b44ec7e

Use the correct instance when showing a fetch error
Some fallout from b8c234576d9e97f2

src/dbp-library-book-list.js
@@ -204,7 +204,7 @@ class LibraryBookList extends ScopedElementsMixin(LibraryElement) {
                 $booksLoadingIndicator.hide();
             })
             .catch((error) => {
-                this.handleFetchError(error, that._i18n.t('book-list.error-load-books'));
+                that.handleFetchError(error, that._i18n.t('book-list.error-load-books'));
                 $booksLoadingIndicator.hide();
             });
     }

src/dbp-library-budget.js
@@ -147,7 +147,7 @@ class LibraryBudget extends ScopedElementsMixin(LibraryElement) {
                     that.pageStatus = pageStatus.noBudget;
                 } else {
                     that.pageStatus = pageStatus.none;
-                    this.handleFetchError(error, that._i18n.t('budget.load-error'));
+                    that.handleFetchError(error, that._i18n.t('budget.load-error'));
                 }
             });
     }

src/dbp-library-loan-list.js
@@ -167,7 +167,7 @@ class LibraryLoanList extends ScopedElementsMixin(LibraryElement) {
                 $loansLoadingIndicator.hide();
             })
             .catch((error) => {
-                this.handleFetchError(error, that._i18n.t('loan-list.error-load-loans'));
+                that.handleFetchError(error, that._i18n.t('loan-list.error-load-loans'));
                 $loansLoadingIndicator.hide();
             });
     }

src/dbp-library-order-list.js
@@ -254,7 +254,7 @@ class LibraryOrderList extends ScopedElementsMixin(LibraryElement) {
                 $booksLoadingIndicator.hide();
             })
             .catch((error) => {
-                this.handleFetchError(error, i18n.t('order-list.error-load-orders'));
+                that.handleFetchError(error, i18n.t('order-list.error-load-orders'));
                 $booksLoadingIndicator.hide();
             });
     }

src/dbp-library-renew-loan.js
@@ -258,7 +258,7 @@ class LibraryRenewLoan extends ScopedElementsMixin(LibraryElement) {
                         $loansLoadingIndicator.hide();
                     })
                     .catch((error) => {
-                        this.handleFetchError(error, i18n.t('renew-loan.error-load-loans-summary'));
+                        that.handleFetchError(error, i18n.t('renew-loan.error-load-loans-summary'));
                         $loansLoadingIndicator.hide();
                     });
 

src/dbp-library-return-book.js
@@ -132,7 +132,7 @@ class LibraryReturnBook extends ScopedElementsMixin(LibraryElement) {
                             }
                         })
                         .catch((error) => {
-                            this.handleFetchError(
+                            that.handleFetchError(
                                 error,
                                 i18n.t('renew-loan.error-load-loans-summary')
                             );
@@ -173,7 +173,7 @@ class LibraryReturnBook extends ScopedElementsMixin(LibraryElement) {
                         };
                     },
                     error: (jqXHR, textStatus, errorThrown) => {
-                        this.handleXhrError(jqXHR, textStatus, errorThrown);
+                        that.handleXhrError(jqXHR, textStatus, errorThrown);
                     },
                     complete: function (jqXHR, textStatus, errorThrown) {
                         that._('#send').stop();

src/dbp-library-shelving.js
@@ -161,7 +161,7 @@ class LibraryShelving extends ScopedElementsMixin(LibraryElement) {
                         $bookOfferSelect[0].clear();
                     },
                     error: (jqXHR, textStatus, errorThrown) => {
-                        this.handleXhrError(jqXHR, textStatus, errorThrown);
+                        that.handleXhrError(jqXHR, textStatus, errorThrown);
                     },
                     complete: function (jqXHR, textStatus, errorThrown) {
                         that._('#send').stop();

src/library-book-offer-select.js
@@ -214,7 +214,7 @@ export class LibraryBookOfferSelect extends ScopedElementsMixin(AdapterLitElemen
                         };
                     },
                     error: (jqXHR, textStatus, errorThrown) => {
-                        this.handleXhrError(jqXHR, textStatus, errorThrown);
+                        that.handleXhrError(jqXHR, textStatus, errorThrown);
                     },
                 },
             })