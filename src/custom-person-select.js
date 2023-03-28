34991344b8c949dd88085517cc44fa17d72929d1
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2023-03-28 13:55:31 +0200

    added custom person selector; still requires update of person-select package from toolkit to work

---
+++ src/custom-person-select.js
@@ -0,0 +1,30 @@
+import {PersonSelect} from "@dbp-toolkit/person-select";
+import {ScopedElementsMixin} from '@open-wc/scoped-elements';
+
+export class CustomPersonSelect extends ScopedElementsMixin(PersonSelect) {
+
+    // Search for persons by name requesting local data attribute 'email'
+    buildUrlData(select, params) {
+        let term = params.term.trim();
+        let data = {};
+        data['includeLocal'] = 'email';
+        data['search'] = term;
+
+        return data;
+    }
+
+    // Includes the persons 'email' attribute if available
+    formatPerson(select, person) {
+        let text = person['givenName'] ?? '';
+        if (person['familyName']) {
+            text += ` ${person['familyName']}`;
+        }
+
+        let email = person?.localData?.email;
+        if (email !== undefined && email.length) {
+             text += ` (${email[0]})`;
+        }
+
+        return text;
+    }
+}
