71537af210eba4a8007831997aa6b0f968bfbf22
Manuel Kocher <manuel.kocher@tugraz.at>
2023-04-13 11:43:04 +0200

    Add errorMixin to class LibraryBookOfferSelect

--- src/library-book-offer-select.js
+++ src/library-book-offer-select.js
@@ -12,6 +12,7 @@ import * as commonUtils from '@dbp-toolkit/common/utils';
 import * as commonStyles from '@dbp-toolkit/common/styles';
 import select2CSSPath from 'select2/dist/css/select2.min.css';
 import {AdapterLitElement} from '@dbp-toolkit/common';
+import * as errorUtils from "@dbp-toolkit/common/error";
 
 select2(window, $);
 
@@ -33,6 +34,8 @@ export class LibraryBookOfferSelect extends ScopedElementsMixin(AdapterLitElemen
         this.showReloadButton = false;
         this.reloadButtonTitle = '';
         this.sublibraryIri = '';
+
+        Object.assign(LibraryBookOfferSelect.prototype, errorUtils.errorMixin);
 
         this._onDocumentClicked = this._onDocumentClicked.bind(this);
     }
