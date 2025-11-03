f49de7ec069da37a91413d7ecd8ea4fb2ef93bfa
Christoph Reiter <reiter.christoph@gmail.com>
2025-11-03 13:17:12 +0100

    build: use nodejs builtin globSync
    available in newer nodejs

--- packages/data-table-view/rollup.config.js
+++ packages/data-table-view/rollup.config.js
@@ -1,4 +1,4 @@
-import {globSync} from 'glob';
+import {globSync} from 'node:fs';
 import url from 'url';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
